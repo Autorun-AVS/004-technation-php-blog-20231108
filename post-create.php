9be626e9478d28dbf0830f68b90efd66fa3af138
Autorun-AVS <102346021+Autorun-AVS@users.noreply.github.com>
2023-11-08 17:11:40 +0600

    FEC02- added post-create

--- post-create.php
+++ post-create.php
@@ -18,6 +18,36 @@
 
   <!-- content section  -->
 
+  <div class="container ">
+    <div class="row d-flex justify-content-center mb-4">
+      <div class="col-md-6 ">
+        <div class="card">
+          <div class="card-body">
+          <h5 class="card-title">Create a post</h5>
+            <form>
+              <div class="mb-3">
+                <label for="postImage" class="form-label">Upload Image:</label>
+                <input class="form-control form-control-sm" id="postImage" type="file">
+              </div>
+
+              <div class="mb-3">
+                <label for="postTitle" class="form-label">Title</label>
+                <input type="text" class="form-control" id="postTitle" placeholder="Write Title">
+              </div>
+              <div class="mb-3">
+                <label for="postDetails" class="form-label">Post Details</label>
+                <textarea class="form-control" id="postDetails" rows="4" placeholder="Write Details"></textarea>
+              </div>
+
+              <button type="submit" class="btn btn-primary">Create Post</button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
 
 
   <!-- footer section  -->
